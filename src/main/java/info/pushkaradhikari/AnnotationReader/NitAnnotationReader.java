c4bc096237d286a2524b5e7f0cee226e05a8567e
Pushkar <pushkar.r.adhikari@gmail.com>
2016-11-30 12:21:10 +0530

    renamed aReader to reader

--- src/main/java/info/pushkaradhikari/AnnotationReader/NitAnnotationReader.java
+++ src/main/java/info/pushkaradhikari/AnnotationReader/NitAnnotationReader.java
@@ -10,10 +10,10 @@ import info.pushkaradhikari.Annotations.FetchValue;
 
 public class NitAnnotationReader {
 	
-	public AbstractReader aReader;
+	public AbstractReader reader;
 	
 	public NitAnnotationReader() {
-		aReader = configureReader(FetchValue.class, ReaderType.STRING_PARAMETERIZED_ENUM);
+		reader = configureReader(FetchValue.class, ReaderType.STRING_PARAMETERIZED_ENUM);
 	}
 
 	public AbstractReader configureReader(Class<? extends Annotation> annotation, ReaderType readerType) {
